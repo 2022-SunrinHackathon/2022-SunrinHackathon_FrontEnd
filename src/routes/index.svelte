f917ab176d8027737df36521560eca28a14beef5
wugusaud77 <Port060707@gmail.com>
2021-12-22 14:26:31 +0900

    add:header and footer

---
+++ src/routes/index.svelte
@@ -0,0 +1,45 @@
+<header>
+	<img src="/images/logo.png" alt="" class="logo" />
+	<ul>
+		<li class="selected">
+			<img src="/images/home.svg" alt="" />
+			홈
+		</li>
+		<li>
+			<img src="/images/articles.svg" alt="" />
+			게시판
+		</li>
+		<li>
+			<img src="/images/meal.svg" alt="" />
+			급식
+		</li>
+		<li>
+			<img src="/images/meal.svg" alt="" />
+			시간표
+		</li>
+		<li>
+			<img src="/images/book.svg" alt="" />
+			선린위키
+		</li>
+	</ul>
+	<div class="profile_box">
+		<div class="profile_info">
+			<div class="name">나선린</div>
+			<div class="profile_image" />
+		</div>
+	</div>
+</header>
+<main />
+<footer>
+	<span>제작 및 운영진</span>
+	<ul>
+		<li>백시현</li>
+		<li>표한빈</li>
+		<li>박정우</li>
+		<li>주현명</li>
+		<li>이유민</li>
+		<li>이하람</li>
+		<li>성예건</li>
+	</ul>
+	<copyright>Copyright ⓒ 2022. AEffects All Rights Reserved.</copyright>
+</footer>
