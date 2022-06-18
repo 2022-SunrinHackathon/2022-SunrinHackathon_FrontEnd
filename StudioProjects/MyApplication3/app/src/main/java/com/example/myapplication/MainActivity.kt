1ff5d769c3415c59fc06532d936d6509c4c1449c
iamraram <slyram06@icloud.com>
2022-06-18 16:38:17 +0900

    edcan is very good

---
+++ StudioProjects/MyApplication3/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+
+
+
+    }
+
+
+}
